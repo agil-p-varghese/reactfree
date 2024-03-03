97ac4135079c2356aedc0884ae2aeb3fec09ca67
agil-p-varghese <agilpvargheese0008@gmail.com>
2024-03-03 18:16:33 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap";
+import "popper.js/dist/umd/popper";
+import NavBar from "./App";
+import "./index.css";
+
+var element=<button class="btn btn-danger">hello world</button>; 
+ReactDOM.render(<App/>,document.getElementById("root"));
+
+ 
